d9be7fe8e7a32916062e678b582432d3d617bea5
Mahavigna <mahavigna@student.usm.my>
2025-01-18 00:06:08 +0800

    added Income.hpp

---
+++ INCOMESELECTION/main.hpp
@@ -0,0 +1,17 @@
+#ifndef INCOME_HPP
+#define INCOME_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cctype>
+#include <limits>
+
+//Function Declaration
+void printLine(char ch, int length);
+string getValidString(const string &prompt);
+
+
+using namespace std;
+
+#endif
