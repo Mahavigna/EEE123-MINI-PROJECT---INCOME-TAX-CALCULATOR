769d6b443fdd6af90d9c8f99060a07514106d1cb
syublqs <syublqs@student.usm.my>
2025-01-17 22:15:11 +0800

    progress

--- INCOMESELECTION/main.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_HPP
-#define MAIN_HPP
-
-void inputIncome(double& salary, double& business, double& dividend);
-void printIncomeTable(double salary, double business, double dividend, double totalIncome);
-
-#endif
